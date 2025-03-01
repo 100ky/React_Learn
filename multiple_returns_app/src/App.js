3ad681b5b0f3b37cf9e0becb86be46f15cb5898b
100ky <stoky911@gmail.com>
2025-03-01 09:27:05 +0100

    Multiple returns

---
+++ multiple_returns_app/src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+
+const url = "http://api.open-notify.org/iss-now.json";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data["iss_position"])
+      .then((position) => {
+        console.log(position["latitude"])
+        console.log(position["longitude"])
+      })
+  }, [])
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  } else {
+    return <h2>Finished Loading</h2>
+  }
+
+
+}
+export default App;
