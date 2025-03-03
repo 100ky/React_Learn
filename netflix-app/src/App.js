e420a06428c2595f9e5c39684b7b3f66983fa9ff
100ky <stoky911@gmail.com>
2025-03-03 09:12:26 +0000

    Upgrade netflix app

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -1,12 +1,75 @@
-import Movie from './components/Movie';
-
+import { useState } from 'react';
+import allMovies from './data';
+import categories from './categories';
+import MovieDeleteButton from './components/MovieDeleteButton';
+import AllDeleteButton from './components/AllDeleteButton';
+import ReloadMovieButton from './components/ReloadMovieButton';
+import "./index.css";
 
 const App = () => {
+  const [typeOfMovie, setTypeOfMovie] = useState("komedie");
+  const [movieList, setMovieList] = useState(allMovies);
+
+  const vysledneFilmy = movieList.filter((oneMovie) => {
+    return oneMovie["category"] === typeOfMovie;
+  });
+
+  const deleteOneMovie = (idecko) => {
+    const filteredMovies = movieList.filter((oneMovie) => {
+      return oneMovie.id !== idecko;
+    });
+    setMovieList(filteredMovies);
+  };
+
+  const deleteAllMovies = () => {
+    setMovieList([]);
+  };
+
+  const reloadMovies = () => {
+    setMovieList(allMovies);
+  };
+
   return (
     <div>
-      <Movie />
+      <div className="all-buttons">
+        {categories.map((oneCategory, index) => (
+          <button
+            className="one-button"
+            key={index}
+            onClick={() => setTypeOfMovie(oneCategory)}
+          >
+            {oneCategory}
+          </button>
+        ))}
+      </div>
+
+      <div className="control-buttons">
+        <AllDeleteButton deleteMovie={deleteAllMovies} />
+        <ReloadMovieButton reloadMovie={reloadMovies} />
+      </div>
+
+      <div className="all-movies">
+        {vysledneFilmy.length > 0 ? (
+          vysledneFilmy.map((oneMovie) => {
+            const { id, image, title, age, tags, description } = oneMovie;
+
+            return (
+              <div className="one-movie" key={id}>
+                <img src={image} alt={title} />
+                <h2>{title}</h2>
+                <p className="age">{age}</p>
+                <p className="tags">{tags}</p>
+                <p className="description">{description}</p>
+                <MovieDeleteButton deleteMovie={() => deleteOneMovie(id)} />
+              </div>
+            );
+          })
+        ) : (
+          <p className="no-movies">Žádné filmy k zobrazení</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- netflix-app/src/data.js
+++ netflix-app/src/data.js
@@ -1,84 +1,91 @@
+import image1 from "./images/movie1.png"
+import image2 from "./images/movie2.png"
+import image3 from "./images/movie3.png"
+import image4 from "./images/movie4.png"
+import image5 from "./images/movie5.png"
+import image6 from "./images/movie6.png"
+import image7 from "./images/movie7.png"
+import image8 from "./images/movie8.png"
+
 const allMovies = [
     {
         id: 1,
-        image: "https://picsum.photos/400/225?random=1",
+        image: image1,
         title: "Enola Holmesová 2",
         age: "+13",
         tags: "Vtipný * Napínavý * Rodina",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "komedie"
     },
     {
         id: 2,
-        image: "https://picsum.photos/400/225?random=2",
+        image: image2,
         title: "Enola Holmesová",
         age: "+13",
         tags: "Vtipný * Napínavý * Rodina",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "komedie"
     },
     {
         id: 3,
-        image: "https://picsum.photos/400/225?random=3",
+        image: image3,
         title: "Rocknrolla",
         age: "+16",
-        tags: "Gangster * Napínavý * Násilí",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        tags: "Gangster * Napínavý * Násilí",        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "romantický"
     },
     {
         id: 4,
-        image: "https://picsum.photos/400/225?random=4",
+        image: image4,
         title: "Patriot",
-        age: "+16",  
+        age: "+16",
         tags: "Gangster * Napínavý * Násilí",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "romantický"
     },
+
+
+
+
     {
         id: 5,
-        image: "https://picsum.photos/400/225?random=5",
+        image: image5,
         title: "Telefon pana Harrigana",
         age: "+16",
         tags: "Gangster * Napínavý * Násilí",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "akční"
     },
     {
         id: 6,
-        image: "https://picsum.photos/400/225?random=6",
+        image: image6,
         title: "Stážista",
         age: "+16",
         tags: "Rodinný * Napínavý * Romantický",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "akční"
     },
-    {
+
+{
         id: 7,
-        image: "https://picsum.photos/400/225?random=7",
+        image: image7,
         title: "Život",
         age: "+13",
         tags: "Vesmír * Napínavý * Nebezpečí",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "horor"
     },
     {
         id: 8,
-        image: "https://picsum.photos/400/225?random=8",
+        image: image8,
         title: "Kód za miliardu dolarů",
         age: "+13",
         tags: "IT * Napínavý",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
-    },
-    {
-        id: 9,
-        image: "https://picsum.photos/400/225?random=9",
-        title: "Pracháči",
-        age: "+16",
-        tags: "Dokument * Napínavý",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
-    },
-    {
-        id: 10,
-        image: "https://picsum.photos/400/225?random=10",
-        title: "Whoami",
-        age: "+13",
-        tags: "Gangster * Napínavý * Násilí",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusantium amet nemo minus quo architecto quas",
+        category: "horor"
     },
 ]
+
+
 
 export default allMovies
